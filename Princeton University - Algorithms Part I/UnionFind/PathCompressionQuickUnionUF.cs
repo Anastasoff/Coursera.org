4d9314fa6e472bcaa7c8232544ed1a8f4ef9d669
Martin Anastasov <martin.i.anastasov@gmail.com>
2014-02-02 01:05:49 +0200

    Update

---
+++ Princeton University - Algorithms Part I/UnionFind/PathCompressionQuickUnionUF.cs
@@ -0,0 +1,60 @@
+﻿internal class PathCompressionQuickUnionUF
+{
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public PathCompressionQuickUnionUF(int n)
+    {
+        this.count = n;
+        this.id = new int[n];
+        this.sz = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            this.id[i] = i;
+            this.sz[i] = i;
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.count;
+        }
+    }
+
+    public bool Connected(int p, int q)
+    {
+        return this.Root(p) == this.Root(q);
+    }
+
+    public void Union(int p, int q)
+    {
+        int i = this.Root(p);
+        int j = this.Root(q);
+        if (this.sz[i] < this.sz[j])
+        {
+            this.id[i] = j;
+            this.sz[j] += this.sz[i];
+        }
+        else
+        {
+            this.id[j] = i;
+            this.sz[i] += this.sz[j];
+        }
+
+        this.count--;
+    }
+
+    private int Root(int i)
+    {
+        while (i != this.id[i])
+        {
+            this.id[i] = this.id[this.id[i]]; // one extra line
+            i = this.id[i];
+        }
+
+        return i;
+    }
+}
