a3a1e9991d8adfeef8184654994b6344c1a1e5e2
Martin Anastasov <martin.i.anastasov@gmail.com>
2014-02-02 01:00:24 +0200

    Upload

---
+++ Princeton University - Algorithms Part I/UnionFind/UnionFind/QuickFindUF.cs
@@ -0,0 +1,39 @@
+﻿public class QuickFindUF
+{
+    private int[] id;
+
+    public QuickFindUF(int n)
+    {
+        this.id = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            this.id[i] = i;
+        }
+    }
+
+    public int[] Id
+    {
+        get
+        {
+            return this.id;
+        }
+    }
+
+    public bool Connected(int p, int q)
+    {
+        return this.id[p] == this.id[q];
+    }
+
+    public void Union(int p, int q)
+    {
+        int pId = this.id[p];
+        int qId = this.id[q];
+        for (int i = 0; i < this.id.Length; i++)
+        {
+            if (this.id[i] == pId)
+            {
+                this.id[i] = qId;
+            }
+        }
+    }
+}
