0a9a38151ed1e116503f1946ddd97fa39b7ae864
Martin Anastasov <martin.i.anastasov@gmail.com>
2014-03-05 00:28:50 +0200

    Update

--- Princeton University - Algorithms Part I/04.ElementarySorts/SelectionSort/SelectionSort.cs
+++ Princeton University - Algorithms Part I/04.ElementarySorts/SelectionSort/SelectionSort.cs
@@ -1,16 +1,37 @@
 ﻿namespace SelectionSort
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
-    class SelectionSort
+    public class SelectionSort
     {
-        static void Main()
+        private SelectionSort()
         {
+        }
 
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j].CompareTo(array[min]) < 0)
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(array, i, min);
+            }
+        }
+
+        private static void Swap<T>(T[] array, int i, int min)
+        {
+            T swap = array[i];
+            array[i] = array[min];
+            array[min] = swap;
         }
     }
 }
---
+++ Princeton University - Algorithms Part I/04.ElementarySorts/SelectionSort/Tester.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SelectionSort
+{
+    internal class Tester
+    {
+        private static void Main()
+        {
+            int[] arrayOfIntegers = new int[] { 7, 10, 5, 3, 8, 4, 2, 9, 6 };
+            SelectionSort.Sort(arrayOfIntegers);
+            Console.WriteLine(string.Join(", ", arrayOfIntegers));
+
+            double[] arrayOfDoubles = new double[] { 1.5, 6.8, 3.6, 5.5, 1.5, 0.2 };
+            SelectionSort.Sort(arrayOfDoubles);
+            Console.WriteLine(string.Join(", ", arrayOfDoubles));
+
+            var cars = new Car[] 
+            {
+               new Car("BMW", "E34", 1989),
+               new Car("BMW", "E39", 1998),
+               new Car("BMW", "E60", 2003)
+            };
+            SelectionSort.Sort(cars);
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
