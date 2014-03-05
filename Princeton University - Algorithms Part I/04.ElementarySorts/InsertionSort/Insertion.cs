a6c9c0a05716e180746d77eddd6e1996cdd03d81
Martin Anastasov <martin.i.anastasov@gmail.com>
2014-03-05 13:58:28 +0200

    Add Insertion sort

---
+++ Princeton University - Algorithms Part I/04.ElementarySorts/InsertionSort/Insertion.cs
@@ -0,0 +1,38 @@
+﻿namespace InsertionSort
+{
+    using System;
+
+    public class Insertion
+    {
+        private Insertion()
+        {
+        }
+
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j].CompareTo(array[j - 1]) < 0)
+                    {
+                        Swap(array, j, j - 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void Swap<T>(T[] array, int j, int x)
+        {
+            T swap = array[j];
+            array[j] = array[x];
+            array[x] = swap;
+        }
+    }
+}
---
+++ Princeton University - Algorithms Part I/04.ElementarySorts/InsertionSort/Tester.cs
@@ -0,0 +1,14 @@
+﻿namespace InsertionSort
+{
+    using System;
+
+    internal class Tester
+    {
+        private static void Main(string[] args)
+        {
+            int[] arrayOfIntegers = new int[] { 7, 10, 5, 3, 8, 4, 2, 9, 6 };
+            Insertion.Sort(arrayOfIntegers);
+            Console.WriteLine(string.Join(", ", arrayOfIntegers));
+        }
+    }
+}
