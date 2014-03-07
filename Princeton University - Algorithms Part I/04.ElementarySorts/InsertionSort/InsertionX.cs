2dc903665b0d144164cdcd12634eb2e9c0e8b5e2
Martin Anastasov <martin.i.anastasov@gmail.com>
2014-03-07 11:22:49 +0200

    Add Shell sort

---
+++ Princeton University - Algorithms Part I/04.ElementarySorts/InsertionSort/InsertionX.cs
@@ -0,0 +1,44 @@
+﻿namespace InsertionSort
+{
+    using System;
+
+    public class InsertionX
+    {
+        private InsertionX()
+        {
+        }
+
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int n = array.Length;
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                if (array[i].CompareTo(array[i - 1]) < 0)
+                {
+                    Swap(array, i, i - 1);
+                }
+            }
+
+            for (int i = 2; i < n; i++)
+            {
+                T value = array[i];
+                int j = i;
+                while (value.CompareTo(array[j - 1]) < 0)
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+
+                array[j] = value;
+            }
+        }
+
+        private static void Swap<T>(T[] array, int j, int x)
+        {
+            T swap = array[j];
+            array[j] = array[x];
+            array[x] = swap;
+        }
+    }
+}
---
+++ Princeton University - Algorithms Part I/04.ElementarySorts/ShellSort/Shell.cs
@@ -0,0 +1,42 @@
+﻿namespace ShellSort
+{
+    using System;
+
+    public class Shell
+    {
+        private Shell()
+        {
+        }
+
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+            int n = array.Length;
+
+            int h = 1;
+            while (h < n / 2)
+            {
+                h = 3 * h + 1;
+            }
+
+            while (h >= 1)
+            {
+                for (int i = h; i < n; i++)
+                {
+                    for (int j = i; j >= h && (array[j].CompareTo(array[j - h]) < 0); j -= h)
+                    {
+                        Swap(array, j, j - h);
+                    }
+                }
+
+                h /= 3;
+            }
+        }
+
+        private static void Swap<T>(T[] array, int x, int y)
+        {
+            T swap = array[x];
+            array[x] = array[y];
+            array[y] = swap;
+        }
+    }
+}
