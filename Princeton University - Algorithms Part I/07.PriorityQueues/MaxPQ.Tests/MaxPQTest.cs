bb0df5c6c9b58510be9210dce2d9c184cac67bc7
Martin Anastasov <m4rt00@gmail.com>
2014-03-15 18:11:47 +0200

    Add MaxPQ.Tests

---
+++ Princeton University - Algorithms Part I/07.PriorityQueues/MaxPQ.Tests/MaxPQTest.cs
@@ -0,0 +1,28 @@
+﻿namespace MaxPQ.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MaxPQTest
+    {
+        private MaxPQ<int> pq;
+
+        [TestMethod]
+        public void TestInitialize()
+        {
+            this.pq = new MaxPQ<int>();
+        }
+
+        [TestMethod]
+        public void TestPeekShouldReturnMinimumItem()
+        {
+            this.pq = new MaxPQ<int>();
+            this.pq.Insert(2);
+            this.pq.Insert(1);
+
+            Assert.AreNotEqual(this.pq.DeleteMax(), this.pq.DeleteMax());
+        }
+
+    }
+}
