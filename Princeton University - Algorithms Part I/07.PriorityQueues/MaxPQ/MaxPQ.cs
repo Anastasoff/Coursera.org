b016909ffa5ca5f88c409333c96534bb0346d62e
Martin Anastasov <m4rt00@gmail.com>
2014-03-15 12:12:09 +0200

    Add Priority Queues

---
+++ Princeton University - Algorithms Part I/07.PriorityQueues/MaxPQ/MaxPQ.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxPQ
+{
+    /// <summary>
+    /// The MaxPQ class represents a priority queue of generic keys.
+    /// </summary>
+    public class MaxPQ
+    {
+        
+    }
+}
