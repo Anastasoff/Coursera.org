6c72ae7949345bbf7260d85c0faea123fcfd405c
Martin Anastasov <m4rt00@gmail.com>
2014-03-15 22:14:31 +0200

    Add MinPQ.Tests

---
+++ Princeton University - Algorithms Part I/07.PriorityQueues/MinPQ.Tests/MinPQTest.cs
@@ -0,0 +1,27 @@
+﻿namespace MinPQ.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MinPQTest
+    {
+        private MinPQ<int> pq;
+
+        [TestMethod]
+        public void TestInitializeMinPQ()
+        {
+            this.pq = new MinPQ<int>();
+        }
+
+        [TestMethod]
+        public void TestDeleteMin()
+        {
+            this.pq = new MinPQ<int>();
+            this.pq.Insert(2);
+            this.pq.Insert(1);
+
+            Assert.AreNotEqual(this.pq.DeleteMin(), this.pq.DeleteMin());
+        }
+    }
+}
