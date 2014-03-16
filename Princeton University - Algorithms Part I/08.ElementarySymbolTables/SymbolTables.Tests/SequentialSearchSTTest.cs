77de525acb70e2c0eae9f0aa2b8a9b08de8f2530
Martin Anastasov <m4rt00@gmail.com>
2014-03-16 10:14:08 +0200

    Add UnitTests

---
+++ Princeton University - Algorithms Part I/08.ElementarySymbolTables/SymbolTables.Tests/SequentialSearchSTTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SymbolTables.Tests
+{
+    [TestClass]
+    public class SequentialSearchSTTest
+    {
+        private SequentialSearchST<string, int> st;
+
+        [TestMethod]
+        public void TestInitializeSequentialSearchST()
+        {
+            this.st = new SequentialSearchST<string, int>();
+        }
+    }
+}
